68eb0276e94e84e452a8bfa1451c235e11cc13fa
Andrew Hill <contact@andrewthomashill.co.uk>
2024-10-04 12:29:26 +0200

    Catch touch end

--- src/Controller/Controller.tsx
+++ src/Controller/Controller.tsx
@@ -1,29 +1,30 @@
-import { MouseEvent } from "react";
+import { MouseEvent, TouchEvent } from "react";
 
 const Controller = () => {
-  const handleDown = (event: MouseEvent) => {
+  const handleDown = (event: MouseEvent | TouchEvent) => {
     const keyEvent = new KeyboardEvent('keydown', {
       code: (event.target as HTMLButtonElement).dataset.key,
     });
 
     window.dispatchEvent(keyEvent);
   }
-  const handleUp = (event: MouseEvent) => {
+  const handleUp = (event: MouseEvent | TouchEvent) => {
     const keyEvent = new KeyboardEvent('keyup', {
       code: (event.target as HTMLButtonElement).dataset.key,
     });
 
     window.dispatchEvent(keyEvent);
   }
+
   return (
     <nav className="d-pad">
       <div className="set vertical">
-        <button className="up" data-key="ArrowUp" onPointerDown={handleDown} onPointerUp={handleUp}></button>
-        <button className="down" data-key="ArrowDown" onPointerDown={handleDown} onPointerUp={handleUp}></button>
+        <button className="up" data-key="ArrowUp" onPointerDown={handleDown} onPointerUp={handleUp} onTouchEnd={handleUp}></button>
+        <button className="down" data-key="ArrowDown" onPointerDown={handleDown} onPointerUp={handleUp} onTouchEnd={handleUp}></button>
       </div>
       <div className="set horizontal">
-      <button className="left" data-key="ArrowLeft" onPointerDown={handleDown} onPointerUp={handleUp}></button>  
-      <button className="right" data-key="ArrowRight" onPointerDown={handleDown} onPointerUp={handleUp}></button>
+      <button className="left" data-key="ArrowLeft" onPointerDown={handleDown} onPointerUp={handleUp} onTouchEnd={handleUp}></button>  
+      <button className="right" data-key="ArrowRight" onPointerDown={handleDown} onPointerUp={handleUp} onTouchEnd={handleUp}></button>
       </div>
     </nav>
   )
