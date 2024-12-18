1e57fbc440b81c82ab7ce3eaa34623c8872b0232
Andrew Hill <contact@andrewthomashill.co.uk>
2024-12-18 06:59:48 +0100

    Fix Laguna scenes

--- src/store.ts
+++ src/store.ts
@@ -77,7 +77,7 @@ const useGlobalStore = create<GlobalState>()(() => ({
   isMapFadeEnabled: true,
 
   availableCharacters: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  party: [0, 2, 3],
+  party: [],
   congaWaypointHistory: [],
   playerSpeed: 0,
   playerAnimationIndex: 0,
