e152494d98bfb5eb23b18d7109819c87aff7e0e9
Andrew Hill <contact@andrewthomashill.co.uk>
2025-01-07 16:54:49 +0100

    Restore party

--- src/store.ts
+++ src/store.ts
@@ -81,7 +81,7 @@ const useGlobalStore = create<GlobalState>()(() => ({
   isMapFadeEnabled: true,
 
   availableCharacters: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  party: [],
+  party: [0, 2, 5],
   congaWaypointHistory: [],
   playerSpeed: 0,
   playerAnimationIndex: 0,
